d9656d1e5d1ec0c069a6def20f3667444b90daa7
Oleh Kiurchev <okrchv@gmail.com>
2023-05-19 17:02:07 +0300

    Initial Commit

---
+++ DropboxClient/DropboxClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropboxClientApp.swift
+//  DropboxClient
+//
+//  Created by Oleh Kiurchev on 19.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DropboxClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
